57131550751b33bd0f6740b04db0ea156a68a5b7
Ravipaygan296 <payghanravi362@gmail.com>
2026-08-04 08:58:16 +0530

    add: missing root gradle project files for android-client

--- android-client/app/src/main/java/com/omnisync/agent/Constants.kt
+++ android-client/app/src/main/java/com/omnisync/agent/Constants.kt
@@ -3,8 +3,8 @@ package com.omnisync.agent
 object Constants {
     /**
      * PRE-CONFIGURED SERVER URL
-     * Set your deployed cloud server URL here (e.g., "https://your-backend.onrender.com" or "http://192.168.1.105:5000")
+     * Set your deployed cloud server URL here "https://remote-management-system.onrender.com"
      * Hardcoding this URL means zero typing or setup is required when opening the app on any phone.
      */
-    const val DEFAULT_SERVER_URL = "https://your-backend.onrender.com"
+    const val DEFAULT_SERVER_URL = "https://remote-management-system.onrender.com"
 }
